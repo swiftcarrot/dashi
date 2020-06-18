b15ffc06d05599248675462c31084c67a205dc2b
Wang Zuo <wzuoadjusted@gmail.com>
2020-06-18 10:15:54 +0800

    add scripts

---
+++ scripts/pokemon.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+cd ~/go/src/github.com/swiftcarrot/pokemon
+rm -rf *
+dashi new
+dashi g scaffold pokemon number:string name:string weight_min:string weight_max:string height_min:string height_max:string classification:string max_cp:integer max_hp:integer image:string
+git add .
+git commit -m "build with `dashi version`"
+git push
