0e1258e7a18ca773b4449f002264aa4f83c89bb9
Wang Zuo <wzuoadjusted@gmail.com>
2020-07-06 21:32:13 +0800

    apollo client no cache

--- generators/dashboard/templates/packages/dashboard/src/index.js
+++ generators/dashboard/templates/packages/dashboard/src/index.js
@@ -8,9 +8,35 @@ import { ErrorBoundary } from 'components';
 import { Layout } from 'layouts';
 import { HomePage, GraphiQLPage } from 'pages';
 
+const ENDPOINT =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'https://api.yousite.com/graphql';
+
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql',
+  uri: ENDPOINT + '/graphql',
+  request: (operation) => {
+    const token = localStorage.getItem('token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
 });
+
+// TODO: defaultOptions in constructor not working
+client.defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'network-only',
+    errorPolicy: 'none',
+  },
+
+  query: {
+    fetchPolicy: 'network-only',
+    errorPolicy: 'none',
+  },
+};
 
 const App = () => {
   return (
