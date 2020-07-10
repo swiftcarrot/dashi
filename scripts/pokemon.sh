eb9a428189b3fd60da2ec13b9558d8aa0f75426c
Wang Zuo <wzuoadjusted@gmail.com>
2020-07-11 00:13:53 +0800

    add /api directory

--- scripts/pokemon.sh
+++ scripts/pokemon.sh
@@ -10,6 +10,7 @@ rm -rf *
 dashi new
 dashi g webpacker app
 dashi g scaffold pokemon number:string name:string weight_min:string weight_max:string height_min:string height_max:string classification:string types:strings resistant:strings attacks:json weaknesses:strings flee_rate:float max_cp:integer evolutions:uuids evolution_requirements_amout evolution_requirements_name max_hp:integer image:string
+make
 cd packages/dashboard && yarn build && cd ../..
 cd packages/app && yarn build && cd ../..
 git add .
