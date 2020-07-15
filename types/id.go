1285ad279f29c243a87968b59a892bd36565a683
Wang Zuo <wzuoadjusted@gmail.com>
2020-07-15 09:44:22 +0800

    add types.ID

---
+++ types/id.go
@@ -0,0 +1,40 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+func MarshalID(s string) Marshaler {
+	return WriterFunc(func(w io.Writer) {
+		io.WriteString(w, strconv.Quote(s))
+	})
+}
+
+// TODO: add uuid support?
+func UnmarshalID(v interface{}) (string, error) {
+	switch v := v.(type) {
+	case string:
+		return v, nil
+	case json.Number:
+		return string(v), nil
+	case int:
+		return strconv.Itoa(v), nil
+	case int64:
+		return strconv.FormatInt(v, 10), nil
+	case float64:
+		return fmt.Sprintf("%f", v), nil
+	case bool:
+		if v {
+			return "true", nil
+		} else {
+			return "false", nil
+		}
+	case nil:
+		return "null", nil
+	default:
+		return "", fmt.Errorf("%T is not a string", v)
+	}
+}
