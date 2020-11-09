d40334d58d042a463e52ec76a6f05a59091eb733
Wang Zuo <wzuoadjusted@gmail.com>
2020-11-09 23:16:30 +0800

    fix apollo client defaultOptions

--- generators/dashboard/templates/packages/dashboard/src/index.js
+++ generators/dashboard/templates/packages/dashboard/src/index.js
@@ -35,20 +35,17 @@ const authLink = setContext((_, { headers }) => {
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'none',
+    },
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'none',
+    },
+  },
 });
-
-// TODO: defaultOptions in constructor not working
-client.defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'none',
-  },
-
-  query: {
-    fetchPolicy: 'network-only',
-    errorPolicy: 'none',
-  },
-};
 
 const App = () => {
   return (
